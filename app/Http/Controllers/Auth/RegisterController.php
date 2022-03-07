accc4076fc874e94e43e078009aab10fc3499957
Achmad Sidik Fauzi <asidikfauzi@gmail.com>
2022-03-07 16:10:35 +0700

    register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -50,8 +51,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'nama' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -62,12 +63,42 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\Models\User
      */
-    protected function create(array $data)
+
+    public function register(Request $request)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        $nama = $request->input('nama');
+        $username = $request->input('username');
+        $password = $request->input('password');
+        $confirmpas = $request->input('password_confirm');
+
+        if(empty($nama) || empty($username) || empty($password))
+        {
+            return back()->with('failed', 'please fill your data')->withInput();
+        }
+        else if(strlen($password) < 8)
+        {
+            return back()->with('failed', 'username minimum 8 char')->withInput();
+        }
+        else if($password !== $confirmpas)
+        {
+            return back()->with('failed', 'passwords are not the same')->withInput();
+        }
+
+        $user = User::where('username', $username)->get()->toArray();
+
+        if(count($user) > 0)
+        {
+            return back()->with('failed', 'Username already exists')->withInput();
+        }
+        $hashPassword = Hash::make($password);
+
+        $users = new User();
+        $users->nama = $nama;
+        $users->username = $username;
+        $users->password = $hashPassword;
+        $users->save();
+
+        return back()->with('success', 'Data succesfully saved');
+
     }
 }
