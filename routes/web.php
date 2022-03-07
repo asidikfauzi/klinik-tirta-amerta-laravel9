fc7d2dda83b6e8e13595b2e3276229c945a07525
Achmad Sidik Fauzi <asidikfauzi@gmail.com>
2022-03-07 14:51:47 +0700

    set webroute and set adminMiddleware

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['prefix'=>'admin', 'middleware'=>['isAdmin','auth']], function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+
